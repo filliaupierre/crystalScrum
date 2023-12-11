cddf4b0a1a92d235149f3e4e99fce7287c99466f
Pierre FILLIAU <pierre.filliau@soprasteria.com>
2023-12-11 10:27:26 -0500

    moving all assets

--- src/components/starrySkySection/StarrySky.js
+++ src/components/starrySkySection/StarrySky.js
@@ -4,7 +4,7 @@ import "./letterPositions.css";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import bigPlanetImage from "./assets/planets/bigPlanet.png";
+import bigPlanetImage from "./assets/starrySkyAssets/planets/bigPlanet.png";
 
 import {
   images,
